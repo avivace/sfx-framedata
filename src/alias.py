5d8c665e18aeef320d2b2101fbba2d44d7c454bd
ricki122 <ricki122@users.noreply.github.com>
2020-02-19 00:54:46 +0100

    Update alias.py
    added moves up until b+throw

--- src/alias.py
+++ src/alias.py
@@ -4,10 +4,31 @@ prefixdict = {
 }
 
 aliasdict = {
-	"LP" : ["jab", "lp"],
-	"MP" : ["AAAA"],
-	"Collarbone Breaker" : ["f+mp"],
-
+	"LP" : ["lp", "jab"],
+	"MP" : ["mp", "strong"],
+	"HP" : ["hp", "fierce"],
+	"LK" : ["lk", "short"],
+	"MK" : ["mk", "forward"],
+	"HK" : ["hk", "roundhouse"],
+	"cr.LP" : ["cr.lp", "cr.jab"],
+	"cr.MP" : ["cr.mp", "cr.strong"],
+	"cr.HP" : ["cr.hp", "cr.fierce"],
+	"cr.LK" : ["cr.lk", "cr.short"],
+	"cr.MK" : ["cr.mk", "cr.forward"],
+	"cr.HK" : ["cr.hk", "sweep", "spazzata"],
+	"j.LP" : ["j.lp", "j.jab"],
+	"j.MP" : ["j.mp", "j.strong"],
+	"j.HP" : ["j.hp", "j.fierce"],
+	"j.LK" : ["j.lk", "j.short"],
+	"j.MK" : ["j.mk", "j.forward"],
+	"j.HK" : ["j.hk", "j.roundhouse"],
+	"f+MP" : ["f+mp"],
+	"f+HP" : ["f+hp"],
+	"b+HK" : ["b+hk"],
+	"MP>HP" : ["mp+hp"],
+	"MP>HP>HK" : ["mp+hp+hk"],
+	"throw" : ["lp+lk", "throw"],
+	"b+throw" : ["b+lp+lk", "b+throw"]
 }
 
 def findAlias(text):
