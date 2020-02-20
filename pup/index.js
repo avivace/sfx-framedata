b5c33026394b43cb3887a16368d66a84d05abc90
Antonio Vivace <avivace4@gmail.com>
2020-02-20 23:12:56 +0100

    Restructuring

--- pup/index.js
+++ pup/index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer')
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false})
+  const browser = await puppeteer.launch({headless: false,args: ['--no-sandbox']})
   const page = await browser.newPage()
   await page.goto('https://steamcommunity.com/openid/login?openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.mode=checkid_setup&openid.return_to=https%3A%2F%2Fgame.capcom.com%2Fcfn%2Fsfv%2Fgate%2Fsteamcallback&openid.realm=https%3A%2F%2Fgame.capcom.com&openid.ns.sreg=http%3A%2F%2Fopenid.net%2Fextensions%2Fsreg%2F1.1&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select')
   await page.type('#steamAccountName', "")
