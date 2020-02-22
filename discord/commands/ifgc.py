57df5c86c70540028ceff6f24718b8393851555d
Antonio Vivace <avivace4@gmail.com>
2020-02-22 17:29:08 +0100

    Basic functionality

--- discord/commands/ifgc.py
+++ discord/commands/ifgc.py
@@ -8,6 +8,14 @@ from bs4 import BeautifulSoup
 from fuzzywuzzy import process
 from collections import OrderedDict
 from commands.utilities import memoize, get_request, register
+import sys
+
+sys.path.append('../')
+
+import alias
+
+with open('../data.json', 'r') as f:
+    data = json.load(f)
 
 class Frames():
 
@@ -342,53 +350,10 @@ class Frames():
 
     @register('!frames')
     async def get_frames(self, msg, user, *args, **kwargs):
-        '''
-        Main method thats called for the frame data function.
-        Currently works only for SFV data thanks to Pauls nicely
-        formatted data <3.
-        '''
-        # Check if they want verbose output.
-        verbose = False
-        info_result = re.search(self.info_regex, msg)
-        if info_result:
-            verbose = True
-            msg = re.sub(self.info_regex, '', msg).strip()
-        result = re.search(self.regex, msg)
+        matchedmove = alias.resolveMoveName(msg)["move"]
+        print(matchedmove)
+        for move in data["vt1"]:
+            if move["name"] == matchedmove:
+                result = str(move)
 
-        if not result:
-            return ("You've passed me an incorrect format %s. "
-                    "The correct format is !frames character_name "
-                    "[vt1/vt2] move_name") % user
-
-        char_name = result.group(1)
-        move_name = result.group(3)
-        if result.group(2):
-            # If either of the vtriggers matched, then we will
-            # pass the number of the matched one.
-            vtrigger = result.group(2)[-1]
-        else:
-            vtrigger = False
-
-        frame_data = await self.get_data(**kwargs)
-        if not frame_data:
-            return 'Got an error when trying to get frame data :(.'
-
-        matched_value = self.match_move(char_name, move_name,
-                                        vtrigger, frame_data)
-        if not matched_value:
-            return ("Don't waste my time %s. %s with %s is not a valid "
-                    "character/move combination for SFV.") % (user,
-                                                              char_name,
-                                                              move_name)
-        else:
-            char, move, data = matched_value
-            text_output = self.format_output(
-                char, move, vtrigger, data, frame_data, move_name
-            )
-            if verbose and 'char_stat' not in data:
-                embed_output = self.format_embeded_message(
-                    char, move, vtrigger, data
-                )
-                return self.add_custom_fields(data, text_output, embed_output)
-            else:
-                return text_output
+        return result
