d403856562b4822d228dd36a226249e4d3756f40
Antonio Vivace <avivace4@gmail.com>
2020-02-24 01:40:32 +0100

    Basic webapp

---
+++ sfxweb/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img width="96px" alt="Vue logo" src="./assets/logo.png">
+    <SFX />
+  </div>
+</template>
+
+<script>
+import SFX from './components/SFX.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SFX
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ sfxweb/src/components/SFX.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>SFX Frame Data</h1>
+    <footer>
+    Data, assets &copy 2018-2020 Capcom.<br>
+    Feedback and support - Source Code - Discord Bot - API</footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 10px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
