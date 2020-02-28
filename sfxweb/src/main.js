991f0cdcc968ff0b680a1d62eb47c08d40baaf04
Denni Bevilacqua <dennib@live.it>
2020-02-28 23:45:59 +0100

    Webapp page title and favicon

--- sfxweb/src/main.js
+++ sfxweb/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// TO DO: Replace titles with Vue router
+document.title = "SFX Frame Data"
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: function (h) { return h(App) },
+  render: function (h) { return h(App) }, 
 }).$mount('#app')
