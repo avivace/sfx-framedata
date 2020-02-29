ce7df54d00b896bec8fc0adc687f46e2f4bd5f7f
ricki122 <ricki122@users.noreply.github.com>
2020-02-29 23:36:35 +0100

    added vega, mika, rashid and karin

--- discord/alias.py
+++ discord/alias.py
@@ -195,6 +195,140 @@ necalliexact = {
     # "Heart Of Gold"
 }
 
+vegaexact = {
+    # CHARACTER: VEGA
+    # V-TRIGGER 1   
+
+    "Matador Flash": ["hp>hp>vs1","hp>hp>vskill1"],
+    "Matador Flash (Attack)": ["hp>hp>vs1 hold","hp>hp>vskill1 hold"],
+    "Rainbow Suplex": ["throw"],
+    "Crescent Line": ["bthrow", "b+throw", "back throw"],
+    "Stardust Shot": ["jthrow", "j.throw", "air throw"],
+    "[VS1] Matador Turn": ["vs1","vskill1"],
+    "[VS1] Matador Turn (Hold Button)": ["vs1 hold","vskill1 hold"],
+    "[VS2] Matador Flip": ["vs2","vskill2"],
+    "[VS2] Cosmic Smart": ["vs2>k","vskill2>k"],
+    # "Bloody Kiss - Torero (Projectile)"
+    "Bloody Kiss - Torero (Attack)": ["vt1", "vtrigger1"],
+    # "Bloody Kiss - Rojo (Projectile)"
+    "Bloody Kiss - Rojo (Attack)": ["d+vt1", "d+vtrigger1"],
+    # "Bloody Kiss - Azul (Projectile)"
+    "Bloody Kiss - Azul (Attack)": ["j.vt1", "j.vtrigger1"],
+    "Backslash": ["vreversal+p","vrev+p","vreversal p","vrev p"],
+    "Short Backlash": ["vreversal+k","vrev+k","vreversal k","vrev k"],
+    "Izuna Drop": ["dp+k>throw"],
+    "EX Izuna Drop": ["dp+kk>throw"],
+    "Bloody Garden Drop": ["du+kk>throw"],
+    "Bloody Rain": ["ca", "critical art", "super"],
+
+    # V-TRIGGER 2
+
+    "Alegrias": ["vt2", "vtrigger2"],
+    # "Flash Arch - Rossa (Stance)"
+    "Flash Arch - Rossa (Attack)": ["vt2 vt2"],
+    "Flash Arch - Granate": ["f+vt2", "f+vtrigger2"]
+}
+
+rmikaexact = {
+    # CHARACTER: R. MIKA
+    # V-TRIGGER 1   
+
+    "Passion Press": ["b+mp","f+mp"],
+    "Passion Rope Throw": ["b+mp>b+mp","f+mp>f+mp","b+mp>f+mp","f+mp>b+mp"],
+    "Daydream Headlock": ["throw"],
+    "Sell Down": ["bthrow", "b+throw", "back throw"],
+    "Dream Driver": ["throw cr"],
+    "[VS1] Mic Performance": ["vs1","vskill1"],
+    "[VS2] Pumped Up!": ["vs2","vskill2"],
+    "[VS2] Pumped Up! (upon successful parry)": ["vs2 parry","vskill2 parry"],
+    "Nadeshiko (above)": ["vt1", "vtrigger1"], 
+    "Nadeshiko (above) (Hold Button)": ["vt1 hold", "vtrigger1 hold"],
+    "Nadeshiko (front)": ["b+vt1", "b+vtrigger1"],
+    "Nadeshiko (front) (Hold Button)": ["b+vt1 hold", "b+vtrigger1 hold"],
+    "Nadeshiko (behind)": ["f+vt1", "f+vtrigger1"],
+    "Nadeshiko (behind) (Hold Button)": ["f+vt1 hold", "f+vtrigger1 hold"],
+    "Peach Gator": ["vreversal","vrev"],
+    "Peach Assault": ["ca", "critical art", "super"],
+
+    # V-TRIGGER 2
+
+    "Steel Chair": ["vt2", "vtrigger2"],
+    "Fightin' Dirty": ["vt2 hold", "vtrigger2 hold"]
+}
+
+rashidexact = {
+    # CHARACTER: RASHID
+    # V-TRIGGER 1   
+
+    "Riding Glider": ["throw"],
+    "Rising Sun": ["bthrow", "b+throw", "back throw"],
+    "[VS1] Front Flip": ["vs1","vskill1"],
+    "[VS1] L Airborne Eagle Spike (from V-Skill)": ["vs1>lk"],
+    "[VS1] M Airborne Eagle Spike (from V-Skill)": ["vs1>mk"],
+    "[VS1] H Airborne Eagle Spike (from V-Skill)": ["vs1>hk"],
+    "[VS1] EX Airborne Eagle Spike (from V-Skill)": ["vs1>kk"],
+    "[VS1] Rolling Assault": ["d+vs1","d+vskill1"],
+    "[VS1] Nail Assault": ["d+vs1>k"],
+    "[VS2] Wing Stroke": ["vs2","vskill2"],
+    "[VS2] Wing Spike": ["vs2>k"],
+    "[VS2]EX Wing Spike": ["vs2>kk"],
+    "[VS2] Airborne Wing Stroke": ["j.vs2","jvs2"],
+    "Ysaar": ["vt1", "vtrigger1"], 
+    "Sliding Roll": ["vreversal","vrev"],
+    "L S2pinning Mixer (Rapid Inputs)": ["qcf+lp lv2"],
+    "L Spinning Mixer (Additional Rapid Inputs)": ["qcf+lp lv3"],
+    "M Spinning Mixer (Rapid Inputs)": ["qcf+mp lv2"],
+    "M Spinning Mixer (Additional Rapid Inputs)": ["qcf+mp lv3"],
+    "H Spinning Mixer (Rapid Inputs)": ["qcf+hp lv2"],
+    "H Spinning Mixer (Additional Rapid Inputs)": ["qcf+hp lv3"],
+    "Dash Spinning Mixer (Rapid Inputs)": ["dash f+p lv2"],
+    "Dash Spinning Mixer (Additional Rapid Inputs)": ["dash f+p lv3"],
+    "Spinning Mixer": ["qcf+p vt1"],
+    "EX Spinning Mixer (Rapid Inputs)": ["qcf+pp lv2"],
+    "EX Spinning Mixer (Additional Rapid Inputs)": ["qcf+pp lv3"],
+    "EX Spinning Mixer": ["qcf+pp vt1"],
+    "EX Airborne Eagle Spike": ["qcb+k vt1"],
+    "Altair": ["ca", "critical art", "super"],
+
+    # V-TRIGGER 2
+
+    "Easifa": ["vt2", "vtrigger2"]
+}
+
+karinexact = {
+    # CHARACTER: KARIN
+    # V-TRIGGER 1   
+
+    "Hajotsui": ["throw"],
+    "Arakuma Inashi": ["bthrow", "b+throw", "back throw"],
+    "[VS1] Meioken": ["vs1","vskill1"],
+    "[VS1] Meioken (Hold Button)": ["vs1 hold","vskill1 hold"],
+    "[VS2] Fudo Sosho": ["vs2","vskill2"],
+    "[VS2] Fudo Sosho (Hold Button)": ["vs2 hold","vskill2 hold"],
+    "Kanzuki-Ryu Guren No Kata": ["vt1", "vtrigger1"],
+    "Ressencho": ["vreversal","vrev"],
+    "Tenko": ["qcf+k>p"],
+    "Tenko (Fastest)": ["instant tenko"],
+    "EX Tenko": ["qcf+kk>p"],
+    "Orochi": ["qcf+k>d+p","qcf+k>dp"],
+    "EX Orochi": ["qcf+kk>d+p","qcf+kk>dp"],
+    "Senha Resshu": ["qcb+p>u+k","qcb+p>uk"],
+    "Senha Kusabi": ["qcb+p>d+k","qcb+p>dk"],
+    "Guren Hosho": ["qcf+p>p"],
+    "Guren Senha": ["qcf+p>u+p","qcf+p>up"],
+    "Guren Hochu": ["qcf+p>d+p>d+p","qcf+p>dp>dp"],
+    "Guren Resshu": ["qcf+p>u+k","qcf+p>uk"],
+    "Guren Kusabi": ["qcf+p>d+k","qcf+p>dk"],
+    "Guren Kyoho": ["qcf+p>k"],
+    "Kanzuki-Ryu Hadorokushiki Hasha No Kata": ["ca", "critical art", "super"],
+
+    # V-TRIGGER 2
+
+    "Tenha No Kata": ["vt2", "vtrigger2"],
+    "Yasha Gaeshi Ten": ["vt2 parry high","vt2 parry mid"],
+    "Yasha Gaeshi Chi": ["vt2 parry low"]
+}
+
 sethexact = {
     # CHARACTER: SETH
     # V-TRIGGER 1   
@@ -271,6 +405,14 @@ def resolveMoveName(userstring):
             result = matchExact(move, kenexact)
         elif char == "necalli":
             result = matchExact(move, necalliexact)
+        elif char == "vega":
+            result = matchExact(move, vegaexact)
+        elif char == "rmika":
+            result = matchExact(move, rmikaexact)
+        elif char == "rashid":
+            result = matchExact(move, rashidexact)
+        elif char == "karin":
+            result = matchExact(move, karinexact)
         elif char == "seth":
             result = matchExact(move, sethexact)
 
